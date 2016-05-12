86eb38c89fcb83638e5d50fa7dde6911383cf00a
jerboar <domotor.andras@compargo.hu>
2016-05-12 21:44:59 +0200

    mongoose

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var up = require('./mymoduls/mymod1')
+var up = require('./mymoduls/mymod1');
 var app = express();
 var port = 3000;
 
-console.log(up.toUpper('hello world'))
+console.log(up.toUpper('hello world'));
+app.use(express.static("build"));
 app.get('/', function (req, res) {
-    fs.readFile('/index.html', 'utf8', function (err, data) {
+    fs.readFile('./build/index.html', 'utf8', function (err, data) {
         if (err) throw err;
         res.send(data);
     });
