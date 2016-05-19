4a92d1b608124f7d563860d58800a72b817a3569
jerboar <domotor.andras@compargo.hu>
2016-05-19 15:51:11 +0200

    user 1. run

--- moduls/users.js
+++ moduls/users.js
@@ -1,43 +1,53 @@
 // felhasználó kollektció kezelése
 
 var db,
-    Users;
+  Users;
 // adatbázishoz kapcsolódás
 function setConnection(mongodb) {
-    db = mongodb;
-    setModell();
+  db = mongodb;
+  setModell();
 }
 
 function setModell() {
-    Users = db.model('users', {
-        name: String,
-        email: String,
-        order: {
-            date: Date,
-            amount: Number,
-            status: String,
-            product: String
-        }
-    }, 'users');
+  Users = db.model('users', {
+    name: String,
+    email: String,
+    phone: String,
+    address: String,
+    role: Number,
+    meta: {
+      birthday: Date,
+      hobby: String,
+    }
+  }, 'users');
 }
 // új dokumentum beszúrása a kollekcióba
-function creat(document) {
-    var user = new Users(document);
-    user.save();
+function create(document, callBack) {
+  var user = new Users(document);
+  user.save(function (err) {
+    if (err) {
+      console.error(err);
+      callBack({});
+    } else {
+      callBack(user);
+    }
+  });
+
 }
 // feltételnek megfelelő rekordok kiolvasása
 function read(where, callBack) {
-    Users.find(where, function (err, data) {
-        if (err) {
-            console.error("Hiba a lekérésben");
-            callBack({});
-        } else {
-            callBack(data);
-        }
-    });
+  Users.find(where, function (err, data) {
+    if (err) {
+      console.error("Hiba a lekérésben");
+      callBack({});
+    } else {
+      callBack(data);
+    }
+  });
 }
 //Publikus elemek
 module.export = {
-    setConnection: setConnection,
-    read: read
-}
+  setConnection: setConnection,
+  read: read,
+  create: create
+};
--- server.js
+++ server.js
@@ -4,14 +4,29 @@ var mongoose = require('mongoose');
 var user = require('./moduls/users');
 var app = express();
 var port = 3000;
-mongoose.connect('mongodb://localhost/shero')
 
+mongoose.connect('mongodb://localhost/shero');
+
+user.setConnection(mongoose);
+user.create({
+  'name': 'Jonh Doe',
+  'email': 'jd@gmail.com',
+  'phone': '555-555',
+  'address': '1111 Budapest, valami u. 12.',
+  'role': 3,
+  'meta': {
+    'birthday': new Date('1982-10-13'),
+    'hobby': 'golf'
+  }
+}, function (saved) {
+  console.log(saved);
+});
 app.use(express.static("build"));
 app.get('/', function (req, res) {
   fs.readFile('./build/index.html', 'utf8', function (err, data) {
     if (err) throw err;
     res.send(data);
   });
-})
+});
 
 app.listen(port);
